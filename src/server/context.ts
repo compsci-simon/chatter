4abcfc24c14882fdab0153948b2ebc2e11da5e9e
Simon <1simonsteven@gmail.com>
2023-05-09 20:09:45 +0200

    Added context file

---
+++ src/server/context.ts
@@ -0,0 +1,16 @@
+import * as trpc from '@trpc/server'
+import * as trpcNext from '@trpc/server/adapters/next'
+import { NodeHTTPCreateContextFnOptions } from '@trpc/server/dist/adapters/node-http'
+import { IncomingMessage } from 'http'
+import ws from 'ws'
+import { prisma } from "~/server/db";
+
+export const createContext = async (
+  opts:
+    | trpcNext.CreateNextContextOptions
+    | NodeHTTPCreateContextFnOptions<IncomingMessage, ws>,
+) => {
+  return {
+    prisma
+  }
+}
