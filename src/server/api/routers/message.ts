761a884add4a686f0969f409bf8e2c97a355732d
Simon <1simonsteven@gmail.com>
2023-05-10 18:03:39 +0200

    Modified addPost procedure to work better

--- src/server/api/routers/message.ts
+++ src/server/api/routers/message.ts
@@ -1,11 +1,29 @@
+import { observable } from "@trpc/server/observable";
+import EventEmitter from "events";
 import { z } from "zod";
-
+import { Post } from "@prisma/client";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+const ee = new EventEmitter()
 
 export const messageRouter = createTRPCRouter({
   send: publicProcedure
-    .input(z.object({ message: z.string() }))
-    .mutation(({ input }) => {
-      return input
+    .input(z.object({ author: z.string(), content: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      const post = await ctx.prisma.post.create({
+        data: { ...input }
+      })
+      ee.emit('onAdd', post)
+      return post
+    }),
+  onAdd: publicProcedure.subscription(() => {
+    return observable<Post>((emit) => {
+      const onAdd = (data: Post) => emit.next(data)
+      ee.on('add', onAdd)
+
+      return () => {
+        ee.off('add', onAdd)
+      }
     })
+  })
 })
