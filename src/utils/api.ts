edff3f4c6ecfa9152708b84be4f1f46d6c9e6736
Simon <1simonsteven@gmail.com>
2023-05-11 17:46:46 +0200

    Made getConfig typesafe

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -7,13 +7,21 @@
 import { httpBatchLink, loggerLink, createWSClient, wsLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
+import { Runtime } from "inspector";
 import { NextPageContext } from "next";
 import getConfig from "next/config";
 import superjson from "superjson";
 
 import { type AppRouter } from "~/server/api/root";
 
-const { publicRuntimeConfig } = getConfig()
+interface RuntimeConfig {
+  APP_URL: string;
+  WS_URL: string;
+}
+
+const { publicRuntimeConfig }: {
+  publicRuntimeConfig: RuntimeConfig
+} = getConfig() as { publicRuntimeConfig: RuntimeConfig }
 
 const { APP_URL, WS_URL } = publicRuntimeConfig
 
