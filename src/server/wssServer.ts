cb130a1395592e828a7f25abec884742dd6d7378
Simon <1simonsteven@gmail.com>
2023-05-13 11:00:03 +0200

    Changed wssServer to be more consistent with the port it says it is using

--- src/server/wssServer.ts
+++ src/server/wssServer.ts
@@ -3,11 +3,11 @@ import { applyWSSHandler } from '@trpc/server/adapters/ws'
 import ws from 'ws'
 import { createContext } from "./context";
 
+const port = 3001
 const wss = new ws.Server({
-  port: 3001
+  port
 })
 
-const handler = applyWSSHandler({ wss, router: appRouter, createContext })
 
 wss.on('connection', (ws) => {
   console.log(`➕➕ Connection (${wss.clients.size})`)
@@ -15,10 +15,9 @@ wss.on('connection', (ws) => {
     console.log(`➖➖ Connection (${wss.clients.size})`)
   })
 })
-console.log(`✅ WebSocket Server listening on ws://localhost:3001`)
+console.log(`✅ WebSocket Server listening on ws://localhost:${port}`)
 
 process.on('SIGTERM', () => {
   console.log('SIGTERM')
-  handler.broadcastReconnectNotification()
   wss.close()
 })
