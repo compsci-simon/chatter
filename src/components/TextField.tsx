df5dde7bdafdfa7b25ace1224ce16746506a1a5a
Simon <1simonsteven@gmail.com>
2023-05-18 20:49:15 +0200

    added suppor to press enter to submit a textfield

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,10 +1,18 @@
-import React, { ChangeEventHandler, FC, InputHTMLAttributes } from 'react';
+import React, { ChangeEventHandler, FC, InputHTMLAttributes, KeyboardEvent } from 'react';
 
-type TextFieldProps = InputHTMLAttributes<HTMLInputElement>
+interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  submithandler?: () => void
+}
 
 const TextField: FC<TextFieldProps> = (props) => {
+
+  const keypress = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter' && props.submithandler) {
+      props.submithandler()
+    }
+  }
   return (
-    <input {...props} className='p-2 outline-violet-500 rounded-md text-lg font-light border border-slate-100 w-full' />
+    <input {...props} onKeyDown={keypress} className='p-2 outline-violet-500 rounded-md text-lg font-light border border-slate-100 w-full' />
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from 'next/navigation';
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Button from "../components/Button";
 import TextField from "../components/TextField";
 
 const Home: NextPage = () => {
   const [name, setName] = useState('')
   const router = useRouter()
+  const submithandler = () => router.push(`/chatroom?username=${name}&chatroom=general`)
 
   return (
     <>
@@ -18,8 +19,8 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="w-[400px] bg-slate-300 rounded-xl flex flex-col p-5 gap-5">
-          <TextField value={name} onChange={e => setName(e.target.value)} />
-          <Button onClick={() => router.push(`/chatroom?username=${name}&chatroom=general`)}>Chat now</Button>
+          <TextField value={name} submithandler={submithandler} placeholder="Your name" onChange={e => setName(e.target.value)} />
+          <Button onClick={submithandler}>Chat now</Button>
         </div>
       </main>
     </>
